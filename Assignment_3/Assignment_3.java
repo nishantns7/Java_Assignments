0b84bee84c47c6eb0c9c83d51a3553dd629eb938
Nishant <nishant.s@zemosolabs.com>
2018-07-06 15:35:02 +0530

    Separating into folders + Assignment 3

---
+++ Assignment_3/Assignment_3.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+class Ping {
+	public static void main(String[] args)throws IOException {
+		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the number of times to ping");
+		int n = Integer.parseInt(buff.readLine());
+		System.out.println("Enter the site/IP address to ping");
+		Process p = Runtime.getRuntime().exec("ping -c " + n + " " + buff.readLine());
+		BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		String s = "" , pingResult = "", time = "";
+		double ping[] = new double[n];
+		int c = 0;
+		while ((s = br.readLine()) != null) {
+			pingResult += s;
+		}
+		String[] fields = pingResult.split(" " , 0);
+		for(String i : fields) {
+			if(i.contains("time=")) {
+				time=i.substring(5);
+				if(time.contains(".")) {
+					ping[c] = Double.parseDouble(time);
+					c++;
+				}
+				else {
+					ping[c] = (double)Integer.parseInt(time);
+					c++;
+				}
+			}
+		}
+		if(n % 2 == 0) {
+			double median = (ping[n/2-1] + ping[n/2]) / 2;
+			System.out.println("The median of the ping times is: " + median);
+		}
+		else
+			System.out.println("The median of the ping times is: " + ping[n/2]);
+	}
+}
