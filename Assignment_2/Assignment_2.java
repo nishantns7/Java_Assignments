5cdfa8cf9a3dd1af0fe77089bc53c6e12026bec7
Nishant <nishant.s@zemosolabs.com>
2018-07-09 23:13:00 +0530

    Revert "Modifications + Tests" Wrong Branch
    This reverts commit 73cc00770159445a13b7f420062c02d6cde10a39.

--- Assignment_2/Assignment_2.java
+++ Assignment_2/Assignment_2.java
@@ -1,41 +1,21 @@
 import java.io.*;
 import java.util.*;
 
-class AlphabetCheck {
-
-/**
-* Takes a string as input from the user and checks to see if it has all the alphabets present in it.
-* Ignores case.
-*/
+class Alphabets {
 	
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the string to be checked");		//User Prompt
 		String str = br.readLine().toLowerCase();		//Reading the user input and converting it to lowercase
-		if(alphaCheck(str))
-			System.out.println("String contains all alphabets.");
-		else
-			System.out.println("String does not contain all alphabets.");
-	}
-	
-	/**
-	* Takes a String as the only argument and converts it into a char array and checks each element to see if all the alphabets have been
-	* encountered.
-	* Returns true if all the alphabets have been encountered, false otherwise.
-	* 
-	* @param str String storing the input by the user
-	*/
-	
-	public static boolean alphaCheck(String str) {
 		HashMap<Character, Boolean> alpha = new HashMap<>();		//Declaring hash table to hold alphabets and their occurence
 		for(char i : str.toCharArray()) {		//Converting string to array of characters and iterating over it
 			if((int)i >= 97 && (int)i <= 122)		//Checking if the ASCII value falls in the range of alphabets
 				alpha.put(i , true);
 		}
 		if(alpha.size() == 26)		//Checking if all alphabets have been encountered
-			return true;
+			System.out.println("String contains all alphabets.");
 		else
-			return false;
+			System.out.println("String does not contain all alphabets.");
 	}
 }
 
--- Assignment_4/KycRangeTester.java
+++ Assignment_4/KycRangeTester.java
@@ -1,31 +1,20 @@
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import static org.junit.Assert.assertEquals;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.time.format.DateTimeFormatter;
 import java.time.Period;
 
-@RunWith(JUnit4.class)
 public class KycRangeTester {
 	
-	@Test
 	public static void main(String[] args) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-		LocalDate entryDate[] = new LocalDate[5];
-		LocalDate currentDate[] = new LocalDate[5];
+		LocalDate entryDate[] = new LocalDate[1];
+		LocalDate currentDate[] = new LocalDate[1];
 		entryDate[0] = LocalDate.parse("16-07-1998" , formatter);
 		currentDate[0] = LocalDate.parse("27-06-2017" , formatter);
-		entryDate[1] = LocalDate.parse("04-02-2016" , formatter);
-		currentDate[1] = LocalDate.parse("04-04-2017" , formatter);
-		entryDate[2] = LocalDate.parse("04-05-2017" , formatter);
-		currentDate[2] = LocalDate.parse("04-04-2017" , formatter);
-		entryDate[3] = LocalDate.parse("04-04-2015" , formatter);
-		currentDate[3] = LocalDate.parse("04-04-2016" , formatter);
-		entryDate[4] = LocalDate.parse("04-04-2015" , formatter);
-		currentDate[4] = LocalDate.parse("15-03-2016" , formatter);
-		assertEquals(KycRange.RangeFinder(entryDate , currentDate , 5 , formatter),"16-06-2017 27-06-2017\n05-01-2017 06-03-2017\nNo range\n05-03-2016 04-04-2016\n05-03-2016 15-03-2016\n");
+		assert KycRange.RangeFinder(entryDate , currentDate , 1 , formatter).equals("16-06-2017 27-06-2017");
 	}
 }
